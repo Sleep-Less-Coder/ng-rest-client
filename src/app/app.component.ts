af43de4f1adfb632d9c16713b4494e55c02b9138
Sleep-Less-Coder <hemant@hemants-mbp.home>
2018-05-20 12:38:14 -0500

    Initial Commitl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  profileName = '';
+  age: number;
+  found: boolean;
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+  onKeyUp(event: any) {
+    this.profileName = event.target.value;
+  }
+
+  getProfile() {
+    console.log(this.profileName);
+    this.httpClient.get(`https://my-json-server.typicode.com/Sleep-Less-Coder/fake-json-rest-endpoint/profiles?name=${this.profileName}`)
+      .subscribe(
+      (data: any[]) => {
+        if (data.length) {
+          console.log(data);
+          this.age = data[0].age;
+          this.found = true;
+        }
+      }
+      );
+  }
+
+  postProfile() {
+    this.httpClient.post(`https://my-json-server.typicode.com/Sleep-Less-Coder/fake-json-rest-endpoint/profiles`,
+      {
+        'name': 'Jason',
+        'age': 23,
+        'country': 'Australia'
+      })
+      .subscribe(
+      (data: any) => {
+        console.log(data);
+      }
+      );
+  }
 }
